4691ebf107f5f66b07231937c25cc8e13026822f
Yaroslav <kharchenko.yar@gmail.com>
2018-08-20 14:51:05 +0700

    coalitions (not working)

--- contracts/loyalty.sol
+++ contracts/loyalty.sol
@@ -4,24 +4,46 @@ import "./coalition.sol";
 import "./token.sol";
 
 contract Loyalty {
-    using SafeMath for uint;
     struct Customer {
         bool exists;
         uint phoneNumber;
         mapping (address => bool) tokens;
     }
     
+    struct Company {
+        bool exists;
+        Token token;
+        string name;
+        uint phoneNumber;
+        Request[] request_pool; 
+        mapping (address => bool) coalitions;
+    }
+    
+    struct Request {
+        string message;
+        address sender;
+        RequestType _type;
+    }
+    
+    struct Coalition{
+        bool exists;
+        string name;
+        address leader;
+        mapping (address => bool) members;
+    }
+    
+    enum RequestType {INVITE}
+    
     // bank address
-    address public owner;
+    address owner;
+    
+    mapping (address => Customer) private customers;
+    mapping (address => Company) public companies;
+    mapping (address => Coalition) public coalitions;
     
     
-    mapping (address => Customer) public customers;
-    mapping (address => Coalition.Company) public companies;
+    //mapping (address => Token) public allTokens;
     
-    // map from company (owner) address to Token
-    mapping (address => Token) public allTokens;
-    
-    // events for debug and output
     event AddCompany(address companyAddress, string name, uint phoneNumber);
     event AddCustomer(address customerAddress, uint number);
     event LoggedIn(address _address, uint number);
@@ -30,7 +52,6 @@ contract Loyalty {
         owner = msg.sender;
     }
     
-    // bank calls
     function addCustomer(address customer, uint _phoneNumber) public
                 onlyOwner
                 customerNotExists(customer)
@@ -40,7 +61,6 @@ contract Loyalty {
         emit AddCustomer(customer, customers[customer].phoneNumber);
     }
     
-    // bank calls
     function addCompany(address company, string _name, uint _phoneNumber) public
                 onlyOwner
                 companyNotExists(company)
@@ -51,53 +71,51 @@ contract Loyalty {
         emit AddCompany(company, companies[company].name, customers[company].phoneNumber);
     }
     
-    // bank calls
-    function transferBonuses(address company,
-                             address customer,
-                             uint roublesAmount,
-                             uint bonusesAmount,
-                             address tokenOwner) // 0 if bonusesAmount == 0
-                                public
-                                onlyOwner
-                                customerExists(customer)
-                                companyExists(company) returns (uint) // if bonusesAmount == 0 returns charged bonuses amount,
-                                                                      // in another case returns roubles amount
-                                {
-        Token token = companies[company].ownToken;
-        // charge bonuses to customer                            
-        if (bonusesAmount == 0) {
-            
-            // the simpliest case - when token belongs to the company
-            if (token.owner() == company) {
-                uint tokensAmount = roublesAmount.mul(token.inPrice());
-                token.transfer(company, customer, tokensAmount);
-                customers[customer].tokens[token] = true;
-                return tokensAmount;
-            }
-            else {
-                // TODO: hard case, needs merge with Slavique
-            }
-        }
-        // write off bonuses
-        else {
-            if (token.owner() == company) {
-                //uint roublesAmount
-            }
-            else {
-                // TODO: hard case, needs merge with Slavique
-            }
-        }
+    
+    
+    function logIn(uint phoneNumber) public {
+        require(customers[msg.sender].phoneNumber == phoneNumber ||
+                companies[msg.sender].phoneNumber == phoneNumber);
+        emit LoggedIn(msg.sender, phoneNumber);
     }
     
-    // company calls
-    // name of the token, tokens per spent rouble, price when you spend tokens
-    function createToken(Token token) public
-        companyExists(msg.sender) {
-        // doesn't exist
-        require(companies[msg.sender].ownToken.owner() == address(0));
-        require(token.owner() == msg.sender);
-        companies[msg.sender].ownToken = token;
-        allTokens[msg.sender] = token;
+    function chargeBonuses(address company, address customer, uint amount) public
+                                onlyOwner
+                                customerExists(customer)
+                                companyExists(company) {
+        companies[msg.sender].token.transfer(company, customer, amount);
+        customers[customer].tokens[companies[company].token] = true;
+    }
+    
+    function createToken(string _name, uint ownerPolicy) public companyExists(msg.sender) {
+        companies[msg.sender].token = new Token(_name, ownerPolicy);
+    }
+    
+    function addCoalition(address coalition, string _name) public 
+                                companyExists(msg.sender)
+                                coalitionNotExists(coalition){
+        coalitions[coalition].exists = true;
+        coalitions[coalition].name = _name;
+        coalitions[coalition].members[msg.sender] = true;
+        coalitions[coalition].leader = msg.sender;
+        companies[msg.sender].coalitions[coalition] = true;
+    }
+    
+    function inviteToCoalition(address coalition) public 
+                                companyExists(msg.sender)
+                                coalitionExists(coalition){
+        Request join_request;
+        join_request.message = "Idi nahui gomofobny pidaras";
+        join_request.sender = msg.sender;
+        join_request._type = RequestType.INVITE;
+        companies[coalitions[coalition].leader].request_pool.push(join_request);
+    }
+    
+    function getRequest () public // вызывать пока не треснешь 
+                            companyExists(msg.sender)
+                            {
+        var request = companies[msg.sender].request_pool[companies[msg.sender].request_pool.length - 1];
+        //return (request.message, request.sender);
     }
     
     modifier onlyOwner() {
@@ -110,18 +128,28 @@ contract Loyalty {
         _;
     }
     
-    modifier companyExists(address company) {
-        require(companies[company].exists, "Company doesn't exist.");
-        _;
-    }
-    
     modifier customerNotExists(address customer) {
         require(!customers[customer].exists, "Customer already exists.");
         _;
     }
     
+    modifier companyExists(address company) {
+        require(companies[company].exists, "Company doesn't exist.");
+        _;
+    }
+    
     modifier companyNotExists(address company) {
         require(!companies[company].exists, "Company already exists.");
+        _;
+    }
+    
+    modifier coalitionExists(address coalition) {
+        require(coalitions[coalition].exists, "Coalition doesn't exist.");
+        _;
+    }
+    
+    modifier coalitionNotExists(address coalition) {
+        require(!coalitions[coalition].exists, "Coalition already exists.");
         _;
     }
 }
