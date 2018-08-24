333cccd37f3a4fdb1de690f9fee2ea75c1e55116
rbkn99 <matveich1903@gmail.com>
2018-08-24 23:09:58 +0700

    upd

--- contracts/loyalty.sol
+++ contracts/loyalty.sol
@@ -4,6 +4,8 @@ import "./token.sol";
 
 contract Loyalty {
     using SafeMath for uint;
+    
+    
     struct Customer {
         bool exists;
         uint phoneNumber;
@@ -12,18 +14,20 @@ contract Loyalty {
     
     struct Company {
         bool exists;
-        address _a;
-        Token token;
-        string name;
-        uint256 deposit;
         uint phoneNumber;
+        string name;
+        address _address;
+        
+        bool has_token;
+        Token token;
+        uint256 deposit;
+        
         Request[] request_pool;
         address[] coalitionNames;
-        mapping (address => bool) coalitions;
+        uint64 request_count;
     }
     
     struct Request {
-        string message;
         address sender;
         RequestType _type;
     }
@@ -31,8 +35,7 @@ contract Loyalty {
     struct Coalition{
         bool exists;
         string name;
-        address leader;
-        mapping (address => bool) members;
+        address[] members;
     }
     
     enum RequestType {INVITE}
@@ -44,12 +47,16 @@ contract Loyalty {
     mapping (address => Customer) public customers;
     mapping (address => Company) public companies;
     mapping (address => Coalition) public coalitions;
-
-    uint64 public companiesCount;
-    Company[] public companySet;
     
     // map from company (owner) address to Token
     mapping (address => Token) public allTokens;
+
+    // for web3 communication
+    uint64 public companiesCount;
+    Company[] public companySet;
+    
+    // cost of asm operations of transferBonuses() func
+    uint constant public transferBonuses_transaction_cost = 119290;
     
     // events for debug and output
     event AddCompany(address companyAddress, string name, uint phoneNumber);
@@ -78,10 +85,11 @@ contract Loyalty {
                 companyNotExists(company)
                 customerNotExists(company) {
         companiesCount++;
-        companies[company]._a = company;
+        companies[company]._address = company;
         companies[company].exists = true;
         companies[company].name = _name;
         companies[company].phoneNumber = _phoneNumber;
+        companies[company].request_count = 0;
         companySet.push(companies[company]);
         emit AddCompany(company, companies[company].name, customers[company].phoneNumber);
     }
@@ -91,12 +99,12 @@ contract Loyalty {
                              address customer,
                              uint roublesAmount,
                              uint bonusesAmount,
-                             address tokenOwner) // 0 if bonusesAmount == 0
+                             address tokenOwner) // 0-address if bonusesAmount == 0
                                 public
                                 onlyOwner
                                 customerExists(customer)
-                                companyExists(company) returns (uint) // if bonusesAmount == 0 returns charged bonuses amount,
-                                                                      // in another case returns roubles amount
+                                companyExists(company) returns (uint) // if bonusesAmount == 0 returns charged bonuses amount * 10^18,
+                                                                      // in another case returns roubles amount * 10^18
                                 {
         uint initialGas = gasleft();
         Token token = companies[company].token;
@@ -105,27 +113,29 @@ contract Loyalty {
             bonusesAmount = roublesAmount.mul(token.inPrice());
             token.transfer(company, customer, bonusesAmount);
             customers[customer].tokens[token] = true;
-            if (!payForTransaction(initialGas - gasleft()))
+            if (!payForTransaction(company, initialGas - gasleft()))
                 revert();
             return bonusesAmount;
         }
         // write off bonuses
         else {
             uint deltaMoney;
-            if (token.owner() == tokenOwner) {
+            if (token.nominal_owner() == tokenOwner) {
                 deltaMoney = bonusesAmount.mul(token.outPrice());
+                roublesAmount = roublesAmount.mul(10^18);
                 roublesAmount = roublesAmount.add(deltaMoney);
                 token.transfer(customer, company, bonusesAmount);
             }
             else {
                 address current_coalition = isMatch(companies[company], 
                                                     companies[tokenOwner]);
-                require(current_coalition != address(0));
+                require(current_coalition != address(0), "Not in one ccoalition");
                 deltaMoney = bonusesAmount.mul(token.exchangePrice());
+                deltaMoney = deltaMoney.mul(token.outPrice());
                 roublesAmount = roublesAmount.add(deltaMoney);
                 token.transfer(customer, tokenOwner, bonusesAmount);
             }
-            if (!payForTransaction(initialGas - gasleft()))
+            if (!payForTransaction(company, initialGas - gasleft()))
                 revert();
             return roublesAmount;
         }
@@ -143,67 +153,158 @@ contract Loyalty {
     }
     
     // company calls
-    // name of the token, tokens per spent rouble, price when you spend tokens
+    // name of the token, tokens per spent rouble, price when you spend tokens, exchange price
     function setToken(string _name, uint _inPrice, uint _outPrice,
                          uint _exchangePrice) public
         companyExists(msg.sender) {
         // doesn't exist => create
-        if (companies[msg.sender].token.owner() == address(0)) {
-            Token token;
+        if (!companies[msg.sender].has_token) {
+            Token token = new Token(msg.sender, _name, _inPrice, _outPrice, _exchangePrice);
             companies[msg.sender].token = token;
             allTokens[msg.sender] = token;
+            companies[msg.sender].has_token = true;
         }
-        //else
-        //    companies[msg.sender].token.updValue(_name, _inPrice, _outPrice, _exchangePrice);
+        else
+            companies[msg.sender].token.updValue(_name, _inPrice,
+                                                _outPrice, _exchangePrice);
     }
     
+    // ethers use for fee payments, company calls
     function addEther() payable public companyExists(msg.sender) {
         companies[msg.sender].deposit = companies[msg.sender].deposit.add(msg.value);
     }
     
-    function payForTransaction(uint gasAmount) private companyExists(msg.sender)
+    event debug(uint gas_A, uint total_eth, uint price);
+    
+    // is called from smart-contract, returns success state
+    function payForTransaction(address company, uint gasAmount) private 
+                                                        companyExists(company)
             returns (bool) { //success
-        uint totalEthAmount = gasAmount * tx.gasprice;
-        if (companies[msg.sender].deposit < totalEthAmount) {
+        uint totalEthAmount = (gasAmount + transferBonuses_transaction_cost) * tx.gasprice;
+        if (companies[company].deposit < totalEthAmount) {
             revert();
             return false;
         }
-        companies[msg.sender].deposit = companies[msg.sender].deposit.sub(totalEthAmount);
+        emit debug(gasAmount, totalEthAmount, tx.gasprice);
+        companies[company].deposit = companies[company].deposit.sub(totalEthAmount);
         owner.transfer(totalEthAmount);
         return true;
     }
     
+    // calls user
+    function getBalanceOf(Token token) public view returns (uint) {
+        return token.balances(msg.sender);
+    }
+    
+    
+    // ---------------------------  exchange /don't thead on me/  -------------------------------
+    
+    function exchangeToken(address customer, Token token1, Token token2, uint amount)
+                                                public onlyOwner customerExists(customer) {
+        require(token1.balances(customer) >= amount, "Not enough bonuses");
+        address current_coalition = isMatch(companies[token1.nominal_owner()], 
+                                                    companies[token2.nominal_owner()]);
+        require(current_coalition != address(0), "Not in one coalition");
+        uint amount2 = amount.mul(token1.exchangePrice());
+        token1.charge(customer, amount);
+        token2.emitToken(customer, amount2);
+    }
+    
+    
+    
 // --------------------------------------------------- NAHUI S MOEGO BOLOTA --------------------------------------------------------------------------    
-    // company calls - it becomes coalition owner
+    // coalition - coalition leader, _name - coalition name
     function addCoalition(address coalition, string _name) public
-                                companyExists(msg.sender)
+                                onlyOwner
+                                companyExists(coalition)
                                 coalitionNotExists(coalition) {
         coalitions[coalition].exists = true;
         coalitions[coalition].name = _name;
-        coalitions[coalition].members[msg.sender] = true;
-        coalitions[coalition].leader = msg.sender;
-        companies[msg.sender].coalitions[coalition] = true;
+        coalitions[coalition].members.push(coalition);
+        companies[coalition].coalitionNames.push(coalition);
     }
-    
-    // falcon calls
-    function inviteToCoalition(address coalition) public 
+    // leader calls. company - company to invite 
+    function inviteToCoalition(address company) public
                                 companyExists(msg.sender)
-                                coalitionExists(coalition) {
+                                coalitionExists(msg.sender)
+                                companyExists(company) {
         Request join_request;
-        join_request.message = "Idi nahui gomofobny pidaras";
         join_request.sender = msg.sender;
         join_request._type = RequestType.INVITE;
-        companies[coalitions[coalition].leader].request_pool.push(join_request);
+        companies[company].request_pool.push(join_request);
+        companies[company].request_count++;
     }
     
-    function getRequest () public // call while not tresnesh' 
+    function getRequestCount() public view
+                            companyExists(msg.sender)
+                            returns (uint request_count){
+        
+        return companies[msg.sender].request_count;
+    }
+ 
+    function getRequestOnIndex (uint64 index) public view
+                            companyExists(msg.sender)
+                            returns (address sender)
+                            {
+            
+        Request request = companies[msg.sender].request_pool[index];
+        return request.sender;
+    }
+    
+    function getCoalitionSize (address coalition) public view 
+                            coalitionExists(coalition)
+                            returns (uint size){
+        return coalitions[coalition].members.length;
+    }
+    
+    function getCoalitionMember(address coalition, uint index) public view 
+                            coalitionExists(coalition)
+                            returns (address member){
+        return coalitions[coalition].members[index];
+    }
+    
+    function getCompanyCoalitionCount() public view 
+                            companyExists(msg.sender)
+                            returns (uint256 size){
+        return companies[msg.sender].coalitionNames.length;
+    }
+    
+    function getCompanyCoalition (uint256 index) public view 
+                            companyExists(msg.sender)
+                            returns (address coalition){
+        return companies[msg.sender].coalitionNames[index];
+    }
+    
+    // to whomstd've and what to respond
+    function respond (address request_sender,  bool answer) public 
                             companyExists(msg.sender)
                             {
-        var request = companies[msg.sender].request_pool[
-            companies[msg.sender].request_pool.length - 1];
-        //TODO: if request is confirmed, push the coalition to company's coalitions
-        //return (request.message, request.sender);
+        bool requestExists;
+        uint request_index;
+        for (uint i = 0; i < companies[msg.sender].request_pool.length; i++){
+            if (companies[msg.sender].request_pool[i].sender == request_sender){
+                requestExists = true;
+                request_index = i;
+            }
+        }
+        require(requestExists, "You\'re trying to asnwer a nonexisting request");
+        if (answer) {
+            coalitions[request_sender].members.push(msg.sender);
+            companies[msg.sender].coalitionNames.push(request_sender);
+        }
+        else {
+            
+        }
+        delete companies[msg.sender].request_pool[request_index];
+        for(i = request_index + 1; 
+            i < companies[msg.sender].request_pool.length;
+            i++){
+                companies[msg.sender].request_pool[i - 1] = companies[msg.sender].request_pool[i];
+        }
+        companies[msg.sender].request_pool.length -= 1;
+        companies[msg.sender].request_count--;
     }
+    
     
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -239,4 +340,5 @@ contract Loyalty {
         require(!coalitions[coalition].exists, "Coalition already exists."); 
         _; 
     }
+    
 }
